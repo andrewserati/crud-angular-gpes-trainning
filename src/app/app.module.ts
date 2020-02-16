86f6bc2fb7abe5d07643c5a84abb3bf2b660eaa2
SirLordX <andrewse2015@hotmail.com>
2020-02-15 21:32:00 -0300

    gerei o componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, PreloadAllModules} from "@angular/router"
+import { HttpModule } from "@angular/http"
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppComponent } from './app.component';
 import { FilmeComponent } from './filme/filme.component';
 
-import { ROUTES } from "./app.routing"
+import { ROUTES } from "./app.routing";
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmeComponent
+    FilmeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules, onSameUrlNavigation: 'reload' })
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
