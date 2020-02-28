579ef6b43e344574d6daf520cb37617e7f303c0b
SirLordX <andrewse2015@hotmail.com>
2020-02-28 12:51:21 -0300

    config addFilme to filme.component

--- src/app/filme/filme.component.ts
+++ src/app/filme/filme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Filme } from './filme.model';
 import { FilmeService } from './filme.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-filme',
@@ -9,13 +11,29 @@ import { FilmeService } from './filme.service';
 })
 export class FilmeComponent implements OnInit {
 
-  Filme: Filme[]
+  Filme: Filme[];
+  productForm: FormGroup;
 
-  constructor(private filmeService: FilmeService) { }
+  constructor(private filmeService: FilmeService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.filmeService.getFilmes().subscribe(res => {
       console.log(res)
+      this.Filme = res['data']
+    })
+
+    this.productForm = this.formBuilder.group({
+      'nome_filme': [null, Validators.required, Validators.maxLength(100)],
+      'descricao_filme': [null, Validators.maxLength(300)],
+      'duracao_filme': [null, Validators.required, Validators.maxLength(20)],
+      'diretor_filme': [null, Validators.required, Validators.maxLength(100)],
+      'genero_filme': [null, Validators.required, Validators.maxLength(50)]
+    })
+
+  }
+
+  addFilme(form: NgForm) {
+    this.filmeService.addFilme(form).subscribe(res => {
       this.Filme = res['data']
     })
   }
